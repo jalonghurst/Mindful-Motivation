6d37333447fbb4c8477b44c9fc8c320830ae8ca6
unknown <jacqui.longhurst@btinternet.com>
2022-07-31 13:49:49 +0200

    Added switch statement for handleChange, and setting default timer values

--- src/components/SetPomodoro.js
+++ src/components/SetPomodoro.js
@@ -1,16 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from './Button'
 
 const SetPomodoro = () => {
+    
+  const [newTimer, setNewTimer] = useState[{
+    // Default values for inputs. active here indicates which time to run by default
+    work: 0.3,
+    short: 0.2,
+    long: 1,
+    active: 'work'
+  }]
+
+  const handleChange = input => {
+    // Destructuring name and value out of right hand value in order to use in switch statement
+    const {name, value} = input.target
+    switch(name) {
+        case 'work':
+            setNewTimer({
+                ...newTimer,
+                work: parseInt(value)
+            })
+            break;
+        case 'shortBreak':
+            setNewTimer({
+                 ...newTimer,
+                short: parseInt(value)
+            })
+            break;
+        case 'longBreak':
+             setNewTimer({
+             ...newTimer,
+             long: parseInt(value)
+        })
+
+        default:
+            break;
+        
+    }
+  }
   return (
     <div className='form-container'>
         <form noValidate>
             <div className='input-wrapper'>
+                {/* Value =  default value unless changed with input */}
                 <input className='input' name="work" onChange={handleChange} value=''></input>
                 <input className='input' name="shortBreak" onChange={handleChange} value=''></input>
                 <input className='input' name="longBreak" onChange={handleChange} value=''></input>
             </div>
-            <Button/>
+            <Button title='Set Timer' _callback={handleSubmit} />
 
         </form>
     </div>
