ec5b830baaa6f52709c1638ecf4dd5c3f001a861
unknown <jacqui.longhurst@btinternet.com>
2022-07-31 12:49:33 +0200

    Input form updated and added Button component

--- src/components/SetPomodoro.js
+++ src/components/SetPomodoro.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import Button from './Button'
 
 const SetPomodoro = () => {
   return (
     <div className='form-container'>
         <form noValidate>
+            <div className='input-wrapper'>
+                <input className='input' name="work" onChange={handleChange} value=''></input>
+                <input className='input' name="shortBreak" onChange={handleChange} value=''></input>
+                <input className='input' name="longBreak" onChange={handleChange} value=''></input>
+            </div>
+            <Button/>
 
         </form>
     </div>
