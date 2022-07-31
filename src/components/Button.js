1451e63e7aebd52f6eeddca93f5495b9fef71497
unknown <jacqui.longhurst@btinternet.com>
2022-07-31 12:37:56 +0200

    Added button component params

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Button = () => {
+const Button = ({title, activeClass, _callback}) => {
   return (
-    <div>Button</div>
+    <button className={activeClass}>{title}</button>
   )
 }
 
